512503a0ab82451777ec1544ea103fd8b83ff498
Nikitas Michalakis <nikitas.michalakis@gmail.com>
2023-03-01 22:32:35 +0200

    First initial commit

---
+++ web/demo/partials/events.php
@@ -0,0 +1,29 @@
+<div id="latest-events" class="py-5 container">
+  <div class="row">
+    <div class="col-lg-4">
+      <div class="events--header">
+        <h2>Οι Δράσεις μας</h2>
+        <p>Ενημερωθείτε σχετικά με τα τελευταία σεμινάρια, μαθήματα, εκδηλώσεις</p>
+        <div class="events--header__footer">
+          <a class="btn-solid-lg" href="#">Προβολή όλων</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4">
+      <div class="event--teaser">
+        <img class="img-fluid" src="images/events-1.jpg" alt="" />
+        <small class="py-3">12 Φεβρουαρίου 2023</small>
+        <h4>Πρόσκληση Συμμετοχής στον Δ' Κύκλο του Προγράμματος Business Launch</h4>
+      </div>
+    </div>
+
+    <div class="col-lg-4">
+      <div class="event--teaser">
+        <img class="img-fluid" src="images/events-2.jpg" alt="" />
+        <small class="py-3">22 Φεβρουαρίου 2023</small>
+        <h4>Παράταση: Υποβολή Αιτήσεων μέχρι 4 Νοεμβρίου</h4>
+      </div>
+    </div>
+
+  </div>
+</div>
---
+++ web/demo/partials/steps.php
@@ -0,0 +1,91 @@
+<div id="startup-steps">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-12">
+        <div class="startup-steps__header text-center py-5">
+          <small class="font-caps startup-steps__mini-header">η διαδικασια</small>
+          <h2>Στάδια Επιλογής</h2>
+          <p>Βρείτε αυτά που χρειάζεστε. Είμαστε εδώ για εσάς. Δίπλα σας σε κάθε βήμα.</p>
+        </div>
+      </div>
+
+      <div class="col-lg-6 mb-grid">
+        <div class="startup-step m-5">
+          <div class="startup-step__number">
+            <span class="startup-step__circle"></span>
+            01
+          </div>
+          <div class="startup-step__content d-flex justify-content-between">
+            <div class="startup-step__icon w-25 pr-3">
+              <i class="fas fa-2x fa-file-invoice"></i>
+            </div>
+            <div class="startup-step__text">
+              <h4>Αίτηση</h4>
+              <p>Αίτηση συμμετοχής, η οποία είναι ανοιχτή σε όλους, αλλά ιδιαίτερα ανταγωνιστική. ίτηση συμμετοχής, η οποία είναι ανοιχτή σε όλους, αλλά ιδιαίτερα ανταγωνιστική.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 mb-grid">
+        <div class="startup-step m-5">
+          <div class="startup-step__number">
+            <span class="startup-step__circle"></span>
+            02
+          </div>
+          <div class="startup-step__content d-flex justify-content-between">
+            <div class="startup-step__icon w-25 pr-3">
+              <i class="fas fa-2x fa-vials"></i>
+            </div>
+            <div class="startup-step__text">
+              <h4>Αξιολόγηση</h4>
+              <p>Ξεκινά με το στάδιο της προαξιολόγησης, δηλαδή τον έλεγχο της πληρότητας της αίτησης ένταξης και του επισυναπτόμενου υλικού τεκμηρίωσης.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 mb-grid">
+        <div class="startup-step m-5">
+          <div class="startup-step__number">
+            <span class="startup-step__circle"></span>
+            03
+          </div>
+          <div class="startup-step__content d-flex justify-content-between">
+            <div class="startup-step__icon w-25 pr-3">
+              <i class="fas fa-2x fa-headset"></i>
+            </div>
+            <div class="startup-step__text">
+              <h4>Συνέντευξη
+              </h4>
+              <p>Πρόκειται για την συνέντευξη της υποψήφιας επιχειρηματικής ομάδας και συνολική αξιολόγηση από ειδικά ορισμένη επιτροπή.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-lg-6 mb-grid">
+        <div class="startup-step m-5">
+          <div class="startup-step__number">
+            <span class="startup-step__circle"></span>
+            04
+          </div>
+          <div class="startup-step__content d-flex justify-content-between">
+            <div class="startup-step__icon w-25 pr-3">
+              <i class="fas fa-2x fa-check-square"></i>
+            </div>
+            <div class="startup-step__text">
+              <h4>Επιλογή
+              </h4>
+              <p>Η σχεδίαση της διαδικασίας επιλογής των startups είναι βασικό στοιχείο του επιταχυντή. Η σχεδίαση της διαδικασίας επιλογής.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
